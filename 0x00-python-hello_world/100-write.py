92cf10ce3d0ef873d5dcf707151a00a311b740aa
alijr2018 <alijr_2018@proton.me>
2023-06-05 16:10:34 -0700

    done

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,10 +2,6 @@
 
 import sys
 
-def main():
-    str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-    sys.stderr.write(str)
-    sys.exit(1)
-
-if __name__ == "__main__":
-    main()
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(str)
+sys.exit(1)
