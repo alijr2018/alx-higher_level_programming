d3dcecdb4ea53fdaeecf1559421289ac09099041
alijr2018 <alijr_2018@proton.me>
2023-06-06 12:33:01 -0700

    done

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-import string
-for i in string.ascii_lowercase:
-    print("{:s}".format(i), end='')
+for i in range(ord('a'), ord('z') + 1):
+    print("{:s}".format(chr(i)), end='')
