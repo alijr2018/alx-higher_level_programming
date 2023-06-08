4c9b8ee2d3e44eb89d90d3bd8d7bcdb8f870e137
alijr2018 <alijr_2018@proton.me>
2023-06-08 15:55:22 -0700

    pycodestyle done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,13 +2,10 @@
 
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
 
-    r_a = add(a, b)
-    r_s = sub(a, b)
-    r_m = mul(a, b)
-    r_d = div(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
