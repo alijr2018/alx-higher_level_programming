c576d8d5e7718436293f51b087b15a2005bc165f
alijr2018 <alijr_2018@proton.me>
2023-06-09 00:16:40 +0100

    lol

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+argv = sys.argv[1:]
+argc = len(argv)
+
+if argc == 0:
+    print("0 arguments.")
+else:
+    print(f"{argc} argument{'s' if argc != 1 else ''}:")
+    for i, arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}")
