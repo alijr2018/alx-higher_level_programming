1549b2f2c0ffcd48e72092f5f16a73ce2346a15f
alijr2018 <alijr_2018@proton.me>
2023-06-09 00:24:41 +0100

    a

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_arg(argv):
+def _arg(argv):
     n = len(argv) - 1
     if n == 0:
         print("{:d} argument.".format(n))
@@ -15,5 +15,7 @@ def print_arg(argv):
             i += 1
 
 if __name__ == "__main__":
+
+
     import sys
-    print_arg(sys.argv)
+    _arg(sys.argv)
