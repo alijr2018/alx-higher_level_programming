8834c15dd99a5f3f4d33e76b5954ebe910d9b4fb
alijr2018 <alijr_2018@proton.me>
2023-06-08 23:45:11 +0100

    done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+
+    r_a = add(a, b)
+    r_s = sub(a, b)
+    r_m = mul(a, b)
+    r_d = div(a, b)
+    print(f"{a} + {b} =", r_a)
+    print(f"{a} - {b} =", r_s)
+    print(f"{a} * {b} =", r_m)
+    print(f"{a} / {b} =", r_d)
