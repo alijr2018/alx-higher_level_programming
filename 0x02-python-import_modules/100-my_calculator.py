61ff97e02a77ba34cd5fe4c11c22efa9d91c5f76
alijr2018 <alijr_2018@proton.me>
2023-06-09 01:26:41 +0100

    l

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,8 +4,7 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print(1)
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     a = int(sys.argv[1])
@@ -23,9 +22,7 @@ if __name__ == "__main__":
         sol = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        print(1)
         sys.exit(1)
 
     print(f"{a} {_operator} {b} = {sol}")
-    print(0)
     sys.exit(0)
