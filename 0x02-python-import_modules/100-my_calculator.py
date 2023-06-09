ed8ee345e0fdf9f42415529b94405c57ae5a116f
alijr2018 <alijr_2018@proton.me>
2023-06-09 01:22:07 +0100

    done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print(1)
         sys.exit(1)
 
     a = int(sys.argv[1])
@@ -24,7 +25,9 @@ if __name__ == "__main__":
         sol = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        print(1)
         sys.exit(1)
 
     print(f"{a} {_operator} {b} = {sol}")
+    print(0)
     sys.exit(0)
