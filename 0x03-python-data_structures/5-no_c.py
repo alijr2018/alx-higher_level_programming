068362d5759eb12d23abbb7be3fee6a35fbe6089
alijr2018 <alijr_2018@proton.me>
2023-06-11 00:24:43 +0100

    done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -9,4 +9,4 @@ def no_c(my_string):
             if my_string[i] != 'c' and my_string[i] != 'C':
                 string += my_string[i]
                 i += 1
-                return (string)
+        return (string)
