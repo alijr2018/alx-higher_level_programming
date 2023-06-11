a1b45a1e0002e82693fe2b7c4f3ba292474b70f6
alijr2018 <alijr_2018@proton.me>
2023-06-11 09:17:54 +0100

    done

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
+
+    f_ele = tuple_a[0] + tuple_b[0]
+    s_ele = tuple_a[1] + tuple_b[1]
+    return (f_ele, s_ele)
