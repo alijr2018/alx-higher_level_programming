c0d5382375d70d42f72cc9718558320cc6e2d45d
alijr2018 <alijr_2018@proton.me>
2023-06-11 10:00:33 +0100

    done

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    i = sentence
     if length <= 0:
-        return (None)
+        i = None
     else:
-        return (length, i)
+        i = sentence[0]
+    return (length, i)
