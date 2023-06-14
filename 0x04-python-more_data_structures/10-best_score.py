80c018e60229ac85537d08ca26dd3e9a2ae8a381
alijr2018 <alijr_2018@proton.me>
2023-06-14 21:09:30 +0100

    done

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+    _i = None
+    _j = float('-inf')
+    for i, j in a_dictionary.items():
+        if j > _j:
+            _i = i
+            _j = j
+    return (_i)
