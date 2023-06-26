71f86f67c1be9945bf1411653d365db39babe380
alijr2018 <alijr_2018@proton.me>
2023-06-26 16:05:55 -0700

    don

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 loop += 1
     except IndexError:
         pass
-    finally:
-        print()
-        return (loop)
+    print()
+    return (loop)
