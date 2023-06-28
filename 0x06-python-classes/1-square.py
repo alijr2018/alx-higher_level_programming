1e761dd30d81e0524a66f9b1ec0bf07df3115c89
alijr2018 <alijr_2018@proton.me>
2023-06-28 01:09:54 +0100

    done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Define a class as a Square."""
+
 
 class Square:
+    """The square."""
     def __init__(self, size):
+        """The def of square."""
         self.__size = size
