260deb62afd540ef5818a57426ec98c29f606b17
alijr2018 <alijr_2018@proton.me>
2023-07-01 11:59:05 +0100

    done

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+def print_square(size):
+    # Check if size is an integer
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    # Check if size is non-negative
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    # Print the square
+    for _ in range(size):
+        print("#" * size)
