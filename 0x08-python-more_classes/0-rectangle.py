959c2289e0448ac8850e1a7a971a75a15d314c48
alijr2018 <alijr_2018@proton.me>
2023-07-03 21:19:15 +0100

    done

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
 class Rectangle:
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    def area(self):
-        return (self.length * self.width)
-
-    def perimeter(self):
-        return (2 * (self.length + self.width))
+    pass
