98e60bc5260a9878b2891d9a869d53c2e2bbe156
alijr2018 <alijr_2018@proton.me>
2023-07-11 00:55:26 +0100

    done

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,21 +4,26 @@ Write a class Rectangle that inherits from BaseGeometry.
 """
 
 
-class Rectangle:
-    """the class"""
-    def __init__(self, width, height):
-        """def init"""
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+class BaseGeometry:
+    """calss of base geometry"""
 
     def area(self):
-        """def area"""
-        return (self.__width * self.__height)
+        """def of area"""
+        raise Exception("area() is not implemented")
 
-    def __str__(self):
-        """def str"""
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+    def integer_validator(self, name, value):
+        """def integer validator"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """the class of rectangle"""
+    def __init__(self, width, height):
+        """def init"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
