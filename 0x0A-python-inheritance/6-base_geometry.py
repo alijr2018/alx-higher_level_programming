09850954a4b97d9359065bacc503855a57951c61
alijr2018 <alijr_2018@proton.me>
2023-07-11 00:33:33 +0100

    done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry (based on 5-base_geometry.py).
+"""
+
+
+class BaseGeometry:
+    """the class"""
+    def area(self):
+        """def of area"""
+        raise Exception("area() is not implemented")
