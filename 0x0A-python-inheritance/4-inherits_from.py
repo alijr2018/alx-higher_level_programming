e87d0c74ab1cd3e17ddf05daca9373924c5c6f6d
alijr2018 <alijr_2018@proton.me>
2023-07-11 00:27:14 +0100

    done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-Write a function that returns True if the object is
-an instance of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False.
+Write a function that returns True otherwise False.
 """
 
 
