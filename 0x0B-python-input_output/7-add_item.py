6f115358b0a8752d10bcda837ee95244db03a593
alijr2018 <alijr_2018@proton.me>
2023-07-12 00:23:20 +0100

    done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,7 +5,6 @@ Write a script that adds all arguments to a Python list,
 and then save them to a file
 """
 import json
-import sys
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
