7c8ae842931c8b4e3eaa58af6498b1b844fafbb9
alijr2018 <alijr_2018@proton.me>
2023-07-12 00:33:11 +0100

    done

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Write a class Student that defines a student
+"""
+
+
+class Student:
+    """class Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """def to_json"""
+        description = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+        return (description)
