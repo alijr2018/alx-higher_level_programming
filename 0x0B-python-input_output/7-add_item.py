3167aa9254e6890be7e927823a53926a9970f33b
alijr2018 <alijr_2018@proton.me>
2023-07-12 00:25:39 +0100

    done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,26 +4,18 @@
 Write a script that adds all arguments to a Python list,
 and then save them to a file
 """
-import json
+import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
-
-def save_to_json_file(my_obj, filename):
-    """def of save_to_json_file"""
-    with open(filename, mode='w') as file:
-        json.dump(my_obj, file)
-
-
-def load_from_json_file(filename):
-    """def of load_from_json_file"""
-    with open(filename) as file:
-        return (json.load(file))
-
-
+# Load existing items from file
 try:
     items = load_from_json_file("add_item.json")
 except Exception:
     items = []
 
+# Add new items from command line arguments
 items.extend(sys.argv[1:])
 
+# Save the updated list to file
 save_to_json_file(items, "add_item.json")
