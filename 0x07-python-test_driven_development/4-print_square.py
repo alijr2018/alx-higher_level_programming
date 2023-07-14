f0c7aa9d007afd83351985cc0f164299801c5e07
alijr2018 <alijr_2018@proton.me>
2023-07-14 17:15:03 +0100

    done

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+"""Write a function that prints a square with the character #."""
+
 
 def print_square(size):
-    # Check if size is an integer
+    """def of print_square"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
 
-    # Check if size is non-negative
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    # Print the square
     for _ in range(size):
         print("#" * size)
