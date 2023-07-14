0dfcdac4a3cd0ff48e09b8839ea3fba2633b7bf7
alijr2018 <alijr_2018@proton.me>
2023-07-14 16:55:50 +0100

    done

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,6 +8,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    if first_name = "":
+    if first_name == "":
         pass
     print("My name is {:s} {:s}".format(first_name, last_name))
