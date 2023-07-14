382a9460e63ff4790b38973396162cfe3747ffe7
alijr2018 <alijr_2018@proton.me>
2023-07-14 16:24:14 +0100

    done

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Write a function that divides all elements of a matrix.
-"""
+"""Write a function that divides all elements of a matrix."""
 
 
 def matrix_divided(matrix, div):
