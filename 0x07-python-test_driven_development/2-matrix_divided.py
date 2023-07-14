fd15afb0f429fc73a0667d102609f9f43c33d3da
alijr2018 <alijr_2018@proton.me>
2023-07-14 16:25:50 +0100

    done

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,20 +4,25 @@
 
 def matrix_divided(matrix, div):
     """def of matrix_divided"""
-    new_matrix = []
-    if type(div) not in [int, float]:
+    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
+    if not matrix:
+        raise TypeError(errorMessage)
+    if not isinstance(matrix, list):
+        raise TypeError(errorMessage)
+    for lists in matrix:
+        if not isinstance(lists, list):
+            raise TypeError(errorMessage)
+        for item in lists:
+            if not isinstance(item, int) and not isinstance(item, float):
+                raise TypeError(errorMessage)
+    for lists in matrix:
+        if len(lists) == 0:
+            raise TypeError(errorMessage)
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
+    if not all(len(lists) == len(matrix[0]) for lists in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    for row in matrix:
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        transpose = []
-        for elem in row:
-            if type(elem) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists)\
- of integers/floats")
-            elem = round(elem / div, 2)
-            transpose.append(elem)
-        new_matrix.append(transpose)
-    return (new_matrix)
+    n_m = [[round(item / div, 2) for item in lists] for lists in matrix]
+    return (n_m)
