24a9e1df204ec2ce10358d19d132d4a82d6bbf2e
alijr2018 <alijr_2018@proton.me>
2023-07-19 23:05:31 +0100

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,10 @@ class Rectangle(Base):
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height
         ))
+
+    def display(self):
+        """Print the Rectangle using '#' character"""
+        for _ in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
