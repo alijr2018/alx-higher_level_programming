dd0f7902c99280d0eda0853326875148bc2d8c9d
alijr2018 <alijr_2018@proton.me>
2023-07-19 22:53:10 +0100

    done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,8 +6,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """class Rectangle"""
+    """Rectangle class that inherits from Base"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize Rectangle instance"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -16,10 +18,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        return (self.__width)
+        """Getter for width attribute"""
+        return self.__width
 
     @width.setter
     def width(self, value):
+        """Setter for width attribute"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -28,10 +32,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """Getter for height attribute"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter for height attribute"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -40,10 +46,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """Getter for x attribute"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """Setter for x attribute"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
@@ -52,41 +60,14 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """Getter for y attribute"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """Setter for y attribute"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
-
-    def area(self):
-        """ public method  def area"""
-        return (self.__width * self.__height)
-
-    def display(self):
-        """  public method def display"""
-        print("\n" * self.__y, end="")
-        for _ in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
-
-    def update(self, *args, **kwargs):
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
-
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def __str__(self):
-        x = {self.__x}/{self.__y}
-        return (f"[Rectangle] ({self.id}) x - {self.__width}/{self.__height}")
