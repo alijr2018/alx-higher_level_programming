c26477f24eec3b1e945b0c456c34bd4a1b5bd1e3
alijr2018 <alijr_2018@proton.me>
2023-07-22 07:43:02 +0100

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,3 +39,17 @@ class Base:
         if json_string is None or len(json_string) == 0:
             return []
         return (json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return instance attributes already set by provided dictionary"""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+        else:
+            dummy_instance = None
+
+        if dummy_instance:
+            dummy_instance.update(**dictionary)
+        return (dummy_instance)
