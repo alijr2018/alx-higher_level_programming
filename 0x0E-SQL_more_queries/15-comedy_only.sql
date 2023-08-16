c895f10d6ddeccbb1b60f4ab2eb4549a492bfd7c
alijr2018 <alirami120@hotmail.fr>
2023-08-16 19:32:42 +0100

    done

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,9 +2,9 @@
 
 SELECT tv_shows.title
 FROM tv_shows
-    JOIN tv_show_genres 
-        ON  tv_show_genres.show_id = tv_show.id
-    JOIN tv_genres 
-        ON tv_genres.id = tv__show_genres.genre_id
+     JOIN tv_show_genres
+     	  ON tv_show_genres.show_id = tv_shows.id
+     JOIN tv_genres
+     	  ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title;
