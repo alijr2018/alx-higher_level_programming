ebff7bc59805e8d6b9c18d7b905dd336dfbad854
alijr2018 <alirami120@hotmail.fr>
2023-08-17 11:31:19 +0100

    done

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,7 +2,6 @@
 
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum
 FROM tv_shows
-JOIN tv_show_ratings 
-    ON tv_shows.id = tv_show_ratings.show_id
+JOIN tv_show_ratings ON tv_shows_ratings.show_id = tv_shows.show_id
 GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
