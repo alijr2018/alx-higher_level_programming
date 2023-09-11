d678175e6c5822a2ef279ef41ef0cbe59f4cacdf
alijr2018 <alirami120@hotmail.fr>
2023-09-11 23:27:53 +0100

    done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2).map(Number);
+const args = process.argv.slice(2);
 
 if (args.length < 2) {
   console.log(0);
 } else {
-  const sortedArgs = args.sort((a, b) => b - a);
-  console.log(sortedArgs[0]);
+  const sortedArgs = args.sort((a, b) => b.slice(0, 2) - a.slice(0, 2));
+  console.log(sortedArgs[1]);
 }
