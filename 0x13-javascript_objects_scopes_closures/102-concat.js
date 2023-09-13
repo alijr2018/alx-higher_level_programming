7df5f87e2d62415b32bf626c9f7fc5a5c1c965e7
alijr2018 <alirami120@hotmail.fr>
2023-09-13 10:15:54 +0100

    done

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,2 +1,36 @@
 #!/usr/bin/node
 
+const fs = require('fs');
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+if (!sourceFile1 || !sourceFile2 || !destinationFile) {
+  console.error('Usage: ./102-concat.js <sourceFile1> <sourceFile2> <destinationFile>');
+  process.exit(1);
+}
+
+fs.readFile(sourceFile1, 'utf8', (err, data1) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+
+  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+    if (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+
+    const concatenatedData = data1 + '\n' + data2;
+
+    fs.writeFile(destinationFile, concatenatedData, 'utf8', (err) => {
+      if (err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+
+      console.log('Concatenation complete.');
+    });
+  });
+});
