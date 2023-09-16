c4f2de3f38f5b046f191cf6b3f879d0c433dcec7
alijr2018 <alirami120@hotmail.fr>
2023-09-16 21:10:44 +0100

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
 
 """
-A script that lists all states from the database hbtn_0e_0_usa.
+a script that lists all states from the database hbtn_0e_0_usa.
 """
 
-import sys
 import MySQLdb
-
+from sys import argv
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
-                         host='localhost', port=3306)
 
-    cursor = db.cursor()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-    sql = """SELECT cities.name
-          FROM states
-          INNER JOIN cities ON states.id = cities.state_id
-          WHERE states.name = %s
-          ORDER BY cities.id ASC"""
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
 
-    cursor.execute(sql, (sys.argv[4],))
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
 
-    data = cursor.fetchall()
-
-    print(", ".join([city[0] for city in data]))
-
-    cursor.close()
+    cur.close()
     db.close()
