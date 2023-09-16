23802ec9485cb371370118456f4f6bfee59c65d9
alijr2018 <alirami120@hotmail.fr>
2023-09-16 14:43:01 +0100

    done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,14 +8,11 @@ from the database hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-# The code should not be executed when imported
 if __name__ == '__main__':
-    # make a connection to the database
+
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states WHERE name\
@@ -24,6 +21,5 @@ if __name__ == '__main__':
     rows = cur.fetchall()
     for i in rows:
         print(i)
-    # Clean up process
     cur.close()
     db.close()
