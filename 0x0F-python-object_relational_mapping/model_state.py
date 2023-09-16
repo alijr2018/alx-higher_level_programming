e169b57c3c9c0badbd4cd1b225848aebcbdb5f50
alijr2018 <alirami120@hotmail.fr>
2023-09-16 21:19:19 +0100

    done

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-    this module contains a Base and State class
+A python file that contains the class definition of a State and,
+an instance Base = declarative_base().
 """
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -15,10 +16,7 @@ Base = declarative_base()
 
 class State(Base):
     """
-        State class inherits the Base class
-        Attributes:
-            id (int)
-            name (string)
+    State class inherits the Base class
     """
     __tablename__ = 'states'
 
