bff530f46d01c0ae51410353727734ffb3186f6f
alijr2018 <alirami120@hotmail.fr>
2023-09-16 21:26:20 +0100

    done

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
 """
-    A script that lists all State objects from the database hbtn_0e_6_usa
-    Username, password and dbname wil be passed as arguments to the script.
+Script that lists all State objects from the database - Using module SQLAlchemy
 """
-
-
-import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
+from sys import argv
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                           sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
 
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
     Base.metadata.create_all(engine)
 
-    # create a session
-    session = Session()
-
-    # extract all states
-    states = session.query(State).order_by(State.id).all()
-
-    # print all states
-    for state in states:
+    s_tate = session.query(State).order_by(State.id).all()
+    for state in s_tate:
         print("{}: {}".format(state.id, state.name))
-
     session.close()
