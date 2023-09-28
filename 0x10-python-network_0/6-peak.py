7cb32d7e9f8752e3102069ca9423d47deaff52f6
alijr2018 <alijr_2018@proton.me>
2023-09-28 23:49:20 +0100

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# interview,a function that finds a peak in a list of unsorted integers.
+"""interview,a function that finds a peak in a list of unsorted integers."""
 
 def find_peak(list_of_integers):
+    """this function it's role is founding te bigger number among the other"""
     if not list_of_integers:
         return None
     value = list_of_integers[0]
