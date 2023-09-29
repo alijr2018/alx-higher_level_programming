c42f3003c3eca184c4379f627bdff216ffe82a7d
alijr2018 <alijr_2018@proton.me>
2023-09-29 22:33:19 +0100

    done

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    rs = requests.get(url)
+    rs_id = rs.header.get('X-Request-Id')
+    print(rs_id)
