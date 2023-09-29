84042ea9feb02a3d7d64fe34865ee3fd6b9961ad
alijr2018 <alijr_2018@proton.me>
2023-09-29 23:14:54 +0100

    done

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a Python script that takes 2 arguments in order to solve this challenge."""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    rep_name = sys.argv[1]
+    own_name = sys.argv[2]
+
+    url = "https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+
+    res = requests.get(url)
+    commits = response.json()
+
+    for commit in commits[:10]:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
