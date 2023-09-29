3ec70a812dde2861faa1273f7fc992137b0e9c17
alijr2018 <alijr_2018@proton.me>
2023-09-29 22:38:04 +0100

    done

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    response = requests.get(url)
-    x_request_id = response.headers.get('X-Request-Id')
+    rs = requests.get(url)
+    resquest_id = rs.headers.get('X-Request-Id')
 
-    print(x_request_id)
+    print(request_id)
