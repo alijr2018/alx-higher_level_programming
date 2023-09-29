2dcc02cf7a867f937bd8ba1376c28e97bbd8d802
alijr2018 <alijr_2018@proton.me>
2023-09-29 23:33:29 +0100

    done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a parameter,
-and finally displays the body of the response."""
+"""
+Script that takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally displays the body
+of the response.
 
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
 import requests
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
+    url = argv[1]
+    value = {"email": argv[2]}
+    req = requests.post(url, data=value)
 
-    # Create the payload with the email
-    payload = {'email': email}
-
-    # Send a POST request with the payload
-    response = requests.post(url, data=payload)
-
-    # Print the email from the response
-    print("Your email is:", email)
+    print(req.text)
