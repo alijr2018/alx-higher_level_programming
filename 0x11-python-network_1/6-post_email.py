db6aad59043bff59c6e2d6c0ac94696d7afeaf77
alijr2018 <alijr_2018@proton.me>
2023-09-29 23:36:28 +0100

    done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    res = requests.post(url, email)
+    i = {'email': email}
+    res = requests.post(url, data=i)
 
     print(res.text)
