9b89de3ba7b7833f3b880eb7ba28e307aa225daa
alijr2018 <alijr_2018@proton.me>
2023-09-29 21:46:50 +0100

    done

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
         content = response.read().decode('utf-8')
         print(content)
-    print("Your email is: {}".format(email))
