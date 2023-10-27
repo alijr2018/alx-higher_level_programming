e3e3031dac4b4e91021ec47dc647c17e654a64d6
alijr2018 <alijr_2018@proton.me>
2023-10-27 15:43:18 +0200

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,15 +4,10 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) =>
-	{
-		if (err)
-		{
-			console.error(err);
-		}
-		else
-		{
-			console.log(data);
-		}
-	});
-
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
