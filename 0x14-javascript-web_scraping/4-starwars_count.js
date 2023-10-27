c24f425d51a3800ce9436b5b645ee767bba03f1a
alijr2018 <alijr_2018@proton.me>
2023-10-27 16:23:51 +0200

    done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,16 +4,28 @@ const request = require('request');
 
 const apiUrl = process.argv[2];
 
+if (!apiUrl) {
+  console.error('Please provide the API URL as an argument.');
+  process.exit(1);
+}
+
 const characterId = 18;
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
-  }else {
+  } else if (response.statusCode !== 200) {
+    console.error(`Request failed with status code ${response.statusCode}`);
+  } else {
     const films = JSON.parse(body).results;
-    const moviesWithWedgeAntilles = films.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-    console.log(moviesWithWedgeAntilles.length);
+    let count = 0;
+
+    films.forEach((film) => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        count++;
+      }
+    });
+
+    console.log(count);
   }
 });
