f9604fcfc57d79b47818e796dbdb1caf2c3f0f77
alijr2018 <alijr_2018@proton.me>
2023-10-27 17:04:56 +0200

    done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -14,6 +14,7 @@ request(apiUrl, (error, response, body) => {
     const characterUrls = movie.characters;
 
     function fetchCharacter (index) {
+      if (index >= characterUrls.length) return;
 
       request(characterUrls[index], (charError, charResponse, charBody) => {
         if (!charError && charResponse.statusCode === 200) {
