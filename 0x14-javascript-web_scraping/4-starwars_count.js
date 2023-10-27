18652a19f38763b10e948b1a3cd72eb6a5c9fbd7
alijr2018 <alijr_2018@proton.me>
2023-10-27 16:38:22 +0200

    done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,19 +4,12 @@ const request = require('request');
 
 const apiUrl = process.argv[2];
 
-const characterId = 18;
-
-request.get(apiUrl, (error, response, body) => {
+request(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Request failed with status code ${response.statusCode}`);
-  } else {
-    const films = JSON.parse(body).results;
-    const count = films.reduce((acc, film) => {
-      return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`) ? acc + 1 : acc;
-    }, 0);
-
-    console.log(count);
   }
+  const nb = JSON.parse(body).results.filter((elem) => {
+    return elem.characters.filter((url) => { return url.includes('18'); }).length;
+  }).length;
+  console.log(nb);
 });
