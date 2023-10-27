d2f6743bad80e89fc37fd870649ea0ca1809d1c8
alijr2018 <alijr_2018@proton.me>
2023-10-27 16:50:20 +0200

    done

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const fs = require('fs');
 
 const url = process.argv[2];
-const filePath = process.argv[3];
 
-if (!url || !filePath) {
-  console.error('Please provide both a URL and a file path as arguments.');
-  process.exit(1);
-}
+const filePath = process.argv[3];
 
 request(url, (error, response, body) => {
   if (error) {
